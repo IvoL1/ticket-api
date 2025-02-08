c4c549a9cb1b2c0a7d49004a92e180a9e0ed0d7c
Ivo <ivoluis239@mail.com>
2025-02-08 01:10:49 -0300

    🔧 feat: Add middleware to validate status in ticket filtering

---
+++ src/middlewares/validateTicketStatus.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import { statusSchema } from '../utils/zod';
+
+export const validateTicketStatus = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { status } = req.query;
+
+  if (status) {
+    const result = statusSchema.safeParse(status);
+    if (!result.success) {
+      res.status(400).json('Status inválido');
+      return;
+    }
+  }
+  next();
+};
